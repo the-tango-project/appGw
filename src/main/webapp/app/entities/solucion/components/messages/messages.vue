a888c62817515c77fefdf73bd6f1192e70038749
danimaniarqsoft <daniel.cortes.pichardo@gmail.com>
2024-09-09 20:40:59 -0600

    adding text-ecitor

--- src/main/webapp/app/entities/solucion/components/messages/messages.vue
+++ src/main/webapp/app/entities/solucion/components/messages/messages.vue
@@ -1,7 +1,18 @@
 <template>
-  <h1>Messages</h1>
-  <br />
-  {{ solution }}
+  <b-card v-if="solution">
+    <b-form-group label-cols="2" :label="$t('archeApp.solucion.seccion.mensaje.bienvenida.solicitante')">
+      <core-text-editor id="mensaje-bienvenida-solicitante" v-model="solution.mensaje.bienvenida"></core-text-editor>
+    </b-form-group>
+    <b-form-group label-cols="2" :label="$t('archeApp.solucion.seccion.mensaje.bienvenida.revisor')">
+      <core-text-editor id="mensaje-bievenida-revisor" v-model="solution.mensaje.bienvenidaRevision"></core-text-editor>
+    </b-form-group>
+    <b-form-group label-cols="2" :label="$t('archeApp.solucion.seccion.mensaje.terminos.revisor')">
+      <core-text-editor id="mensaje-terminos" v-model="solution.mensaje.terminosRevision"></core-text-editor>
+    </b-form-group>
+    <b-form-group label-cols="2" :label="$t('archeApp.solucion.seccion.mensaje.bienvenida.operador')">
+      <core-text-editor id="mensaje-bienvenida-operador" v-model="solution.mensaje.bienvenidaOperador"></core-text-editor>
+    </b-form-group>
+  </b-card>
 </template>
 
 <script lang="ts" src="./messages.component.ts"></script>
